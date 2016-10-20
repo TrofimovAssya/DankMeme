64153b3793e524095b626758afa4d9eed6570784
TrofimovAssya <assya.trofimov@umontreal.ca>
2016-10-20 16:48:16 -0400

    refactoring the code and changing up data processing

--- lstm_meme.py
+++ lstm_meme.py
@@ -8,14 +8,3 @@ from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
 
-#loading data files + dictionaries
-tokenized_sentences = pickle.load(open("tokenized_sentences","rb"))
-word_to_index = pickle.load(open("word_to_index","rb"))
-index_to_word = pickle.load(open("index_to_word","rb"))
-X_train = numpy.load("insoviet_x")
-y_train = numpy.load("insoviet_y")
-
-
-model = Sequential()
-model.add(LSTM(32, input_dim=64, input_length=10))
-
