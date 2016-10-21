cedec7cc7672c4adf3293da10f09833c8744b518
TrofimovAssya <assya.trofimov@umontreal.ca>
2016-10-20 22:07:13 -0400

    rearranging things

--- lstm_meme.py
+++ lstm_meme.py
@@ -1,10 +1,8 @@
-import numpy
-import cPickle as pickle
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import LSTM
-from keras.callbacks import ModelCheckpoint
-from keras.utils import np_utils
+from keras.layers import LSTM, Dense
+import numpy as numpy
 
+
+X_train = np.load("insoviet_x.npy")
+y_train = np.load("insoviet_y.npy")
 
